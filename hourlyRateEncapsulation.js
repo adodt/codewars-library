2eb7736557af7e82d1917a4f9be4d3bbc57d4c1b
Gator <allidodt@gmail.com>
2022-10-17 17:20:33 -0700

    encapsulation example

---
+++ hourlyRateEncapsulation.js
@@ -0,0 +1,8 @@
+let seriousBusinessPerson = {
+    hourlyRate: 250,
+    hours: 160,
+    taxRate: .35,
+    calculateProfit: function(){
+        return this.hourlyRate * this.hours * (1-this.taxRate)
+    }
+}
